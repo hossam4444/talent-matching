1a91deffc167e893c83d064983432253763ebaf8
hosam4444 <hosamtarek4444@gmail.com>
2024-07-17 00:39:10 +0300

    del: cleaning up some files

--- connectors/utils/mongoURI.js
+++
@@ -1,6 +0,0 @@
-module.exports = ({ host, port, user, pass, db }) => {
-  if (pass) {
-    return `mongodb://${user}:${pass}@${host}:${port}/${db}`;
-  }
-  return `mongodb://${host}:${port}/${db}`;
-};
