6e340ad02f712ccc1f99f70efeb96e31b44f5567
hosam4444 <hosamtarek4444@gmail.com>
2024-07-17 00:31:05 +0300

    fix: edited ther eentery point to the conventions

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const express = require("express");
-const mongoConnector = require("./connectors/mongo.connector");
-const redisConnector = require("./connectors/redis.connector");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const redisConnector = require('./connectors/redis.connector');
+const apiRoutes = require('./routes/index');
+
+// Start express app
 const app = express();
 
 // connectors
+// redisConnector.connect();
 
-redisConnector.connect().then(mongoConnector.connect);
+// GLOBAL MIDDLEWARES
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
 
 // loaders
+
+// 3) ROUTES
+app.use('/api', apiRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 module.exports = app;
--- config/env.config.js
+++ config/env.config.js
@@ -1,18 +1,12 @@
-require("dotenv").config();
-const path = require("path");
+require('dotenv').config();
+const path = require('path');
 const root = process.cwd();
 
 module.exports = {
   rootPath: process.cwd(),
-  pjson: require(path.join(root, "./package.json")),
+  pjson: require(path.join(root, './package.json')),
   PORT: process.env.PORT || 3111,
   DATABASE_NAME: process.env.DATABASE_NAME || pjson.name,
-  DATABASE_HOST: process.env.DATABASE_HOST,
-  DATABASE_PORT: process.env.DATABASE_PORT,
   DATABASE_USER: process.env.DATABASE_USER,
   DATABASE_PASS: process.env.DATABASE_PASS,
-  REDIS_PASS: process.env.REDIS_PASS,
-  REDIS_HOST: process.env.REDIS_HOST,
-  REDIS_PORT: process.env.REDIS_PORT,
-  REDIS_USER: process.env.REDIS_USER
 };
